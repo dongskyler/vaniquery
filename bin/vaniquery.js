ec0378519b05cf613b8788a8de309274918f799c
Skyler Dong <dongskyler@gmail.com>
2020-07-21 10:36:55 -0500

    :star: Set up project & add core functionalities (vanilla & revert) (#2)
    * :star: Add core functions & training JS for dev
    
    * :star: Add functions: vanilla files & revert changes
    
    * :star: Add functions: vanilla files & revert changes
    
    * :memo: Update README & CONTRIBUTING

--- bin/vaniquery.js
+++ bin/vaniquery.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
 
-console.log('Hello!');
+/**
+ * Main script of vaniquery
+ */
+
+const revert = require('../lib/revert');
+const vanilla = require('../lib/vanilla');
+
+const thisYear = new Date().getFullYear();
+const copyrightNotice = `Copyright (c) ${thisYear} Badwater Bay`;
+
+require('yargs') // eslint-disable-line
+  .usage('Usage: vaniquery [OPTIONS] COMMAND')
+  .option('c', {
+    alias: 'cache',
+    describe: 'Save changes to cache',
+    type: 'boolean',
+  })
+  // .option('v', {
+  //   alias: 'verbose',
+  //   describe: 'Show the code being worked on',
+  //   type: 'boolean',
+  // })
+  .command({
+    command: 'vanilla',
+    desc: 'Convert jQuery selectors to vanilla JavaScript ones in a file',
+    handler(argv) {
+      vanilla(argv);
+    },
+  })
+  .nargs('vanilla', 1)
+  .command({
+    command: 'revert',
+    desc: 'Revert the changes in a file if the cached file is present',
+    handler: (argv) => {
+      revert(argv);
+    },
+  })
+  .nargs('revert', 1)
+  .epilog(copyrightNotice)
+  .demandCommand(1).argv;
---
+++ training/training_1.js
@@ -0,0 +1,30 @@
+const app = () => {
+  return document.addEventListener('DOMContentLoaded', function () {
+    $('body').bootstrapMaterialDesign();
+
+    (() => {
+      $('#copy-to-username').click(
+        /** @this HTMLElement */
+        function () {
+          $('#target-username').val(() =>
+            $(this).prop('checked') ? $('#target-owner').val() : ''
+          );
+        }
+      );
+
+      $('#target-owner').keyup(() => {
+        $('#copy-to-username').prop('checked') &&
+          $('#target-username').val($('#target-owner').val());
+      });
+
+      $('#target-username').keyup(
+        /** @this HTMLElement */
+        function () {
+          $('#copy-to-username').prop('checked', () => {
+            return $(this).val() === $('#target-owner').val();
+          });
+        }
+      );
+    })();
+  });
+};
