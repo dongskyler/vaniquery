95157da93635377cdd012571f61d1883e34e9c84
Skyler Dong <dongskyler@gmail.com>
2020-07-23 09:34:35 -0500

    :star: Solve click & keyup cases (#28)

---
+++ __tests__/testCases/click.js
@@ -0,0 +1,15 @@
+$('.button').click(function (e) {
+  return false;
+});
+
+$(".button").click(function (e) {
+  return false;
+});
+
+$('.button').click(() => { return false; });
+
+$('#big-button').click(function (e) { return false; });
+
+$("#big-button").click(function (e) { return false; });
+
+$('#big-button').click((e) => { return false; });
---
+++ __tests__/testCases/keyup.answerkey.js
@@ -0,0 +1,15 @@
+document.getElementById('somebody').addEventListener('keyup', () => {
+  // Do something
+});
+
+document.getElementById('somebody').addEventListener('keyup', function (e) {
+  // Do something
+});
+
+document.getElementById('somebody-some').addEventListener('keyup', (event) => {
+  // Do something
+});
+
+document.getElementById("somebody-some").addEventListener("keyup", (event) => {
+  // Do something
+});
