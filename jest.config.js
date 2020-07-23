3e7e0ff2f6bce9870395ce3c78643e2881951cda
Skyler Dong <dongskyler@gmail.com>
2020-07-23 15:19:13 -0500

    Include jest.config in package.json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  verbose: true,
-  testMatch: ['**/__tests__/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  coveragePathIgnorePatterns: ['<rootDir>/__tests__/pendingTestChallenges/.*$'],
-  testPathIgnorePatterns: ['(testCases).*$'],
-};
