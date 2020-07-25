5719a63a6f8e596246a61eb47a824c0d99baed7d
Skyler Dong <dongskyler@gmail.com>
2020-07-25 04:45:48 -0500

    :star: Solve parent & mouseenter

---
+++ __tests__/finishedTestChallenges/mouseenter.test.js
@@ -0,0 +1,8 @@
+/**
+ * Test function vanilla on case 'mouseenter'
+ */
+
+const { testVanillaOnTestCase } = require('../testLib/testCommandOnTestCase');
+
+const testCase = 'mouseenter';
+testVanillaOnTestCase(testCase);
---
+++ __tests__/finishedTestChallenges/parent.test.js
@@ -0,0 +1,8 @@
+/**
+ * Test function vanilla on case 'parent'
+ */
+
+const { testVanillaOnTestCase } = require('../testLib/testCommandOnTestCase');
+
+const testCase = 'parent';
+testVanillaOnTestCase(testCase);
